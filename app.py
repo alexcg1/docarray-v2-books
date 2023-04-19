4df01052da8bdce75f2b745eb6fa37d242e50f20
Alex C-G <alexcg@outlook.com>
2023-04-19 15:11:29 +0200

    feat: start on hnswlib db
    Signed-off-by: Alex C-G <alexcg@outlook.com>

--- app.py
+++ app.py
@@ -1,27 +1,17 @@
 import csv
 import random
 
+import numpy as np
 from docarray import BaseDoc, DocList
 from docarray.documents import ImageDoc, TextDoc
+from docarray.index import HnswDocumentIndex
 
-from helper import Book, clip_encode_desc
-
-book1 = Book(
-    title="Hitchhiker's Guide to the Galaxy",
-    author='Douglas Adams',
-    price=15.99,
-    rating=4.9,
-    desc=TextDoc(
-        text='"The Hitchhiker\'s Guide to the Galaxy" is a beloved science fiction novel that takes readers on a wild and hilarious journey through space. The story follows the misadventures of an unwitting human named Arthur Dent and his alien friend Ford Prefect as they travel through the galaxy, encountering bizarre characters and situations along the way. With its quirky humor and satirical take on the genre, "The Hitchhiker\'s Guide to the Galaxy" has become a classic of science fiction and a must-read for fans of the genre.'
-    ),
-    image=ImageDoc(
-        url='https://m.media-amazon.com/images/I/51MzUz8rQcL._SX305_BO1,204,203,200_.jpg'
-    ),
-)
+from helper import CSV_FILE, Book, clip_encode_desc
 
 books = DocList[Book]()
+db = HnswDocumentIndex[Book](work_dir='workspace')
 
-with open('data/books.csv', newline='') as csvfile:
+with open(CSV_FILE, newline='') as csvfile:
     reader = csv.DictReader(csvfile)
 
     data = list(reader)
@@ -35,13 +25,13 @@ with open('data/books.csv', newline='') as csvfile:
             author=row['author'],
             price=price,
             rating=row['rating'],
-            desc=TextDoc(text=row['desc']),
+            desc=row['desc'],
+            # desc=TextDoc(text=row['desc']),
             image=ImageDoc(url=row['image']),
         )
 
         books.append(doc=book)
 
-books.save_binary('books.bin')
+clip_encode_desc(books)
 
-index = clip_encode_desc(books[0:1])
-print(index[0])
+db.index(books)
--- helper.py
+++ helper.py
@@ -3,21 +3,24 @@ import os
 import requests as rq
 from docarray import BaseDoc, DocList
 from docarray.documents import ImageDoc, TextDoc
+from docarray.typing import NdArray
 from dotenv import load_dotenv
 
 load_dotenv()
 
+CSV_FILE = 'data/books-20.csv'
+
 
 class Book(BaseDoc):
     title: str
+    embedding: NdArray[512] | None
     author: str
     price: float
     rating: float
-    desc: TextDoc
+    # desc: TextDoc
+    desc: str
     image: ImageDoc
 
-
-print(os.getenv('JINA_TOKEN'))
 
 headers = {
     'Content-Type': 'application/json',
@@ -28,16 +31,18 @@ headers = {
 def clip_encode_desc(docs):
     url = 'https://evolving-lacewing-2d90dee9c4-http.wolf.jina.ai/post'
 
-    for doc in docs:
+    for i, doc in enumerate(docs, start=1):
         text_payload = {
-            'data': [{'text': doc.desc.text}],
+            # 'data': [{'text': doc.desc.text}],
+            'data': [{'text': doc.desc}],
             'execEndpoint': '/',
         }
 
+        print(f'Encoding {i}/{len(docs)}')
         response = rq.post(url, json=text_payload, headers=headers)
 
         content = response.json()
         embedding = content['data'][0]['embedding']
-        doc.desc.embedding = embedding
+        doc.embedding = embedding
 
     return docs
